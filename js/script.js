eb8d8e37e20915fc41e5a5c68060cf519576ed11
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-10-16 23:44:37 +0300

    игра. Функция Замыкание

---
+++ js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let isNumber = function(n){
+    return !isNaN(parseFloat(n)) && isFinite(n);
+ };
+
+const game = function(){
+const randomNumber = parseInt((Math.random()*100)+1);
+return  function repeat(){
+   
+  
+        let userNumber = prompt('Введите число ');
+        if (isNumber(userNumber)){
+           if ( +userNumber < randomNumber ){
+               alert ('Загаданное число больше');
+               return repeat();
+           }   else if ( +userNumber > randomNumber ){
+                   alert ('Загаданное число меньше');
+                   return repeat();
+           }
+               else if ( +userNumber === randomNumber ){
+                   alert ('Поздравляю, вы угадали!!');
+                   return repeat();
+      
+           }
+           // проверяет является ли числом
+        }else if (!isNumber(userNumber)){
+            userNumber = confirm('Введите число');
+           //  если нажали Отмена
+            if (userNumber === null) {
+                alert('Пока');
+            }else {
+                return repeat();
+            }
+        }
+     
+}
+
+};
+
+//  запускает функцию на проверку условий 
+game()();
